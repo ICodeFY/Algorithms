e5173d5371e8eee8ff5d2d298861c8aad21c6efc
LearnIT <learnpremiumit@gmail.com>
2017-03-28 08:52:42 +0300

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountDown(10);
+
+            Console.ReadKey();
+        }
+
+        static void CountDown(int n)
+        {
+            if (n <= 0)
+                return;
+
+            Console.WriteLine(n);
+            CountDown(n - 1);
+        }
+    }
+}
